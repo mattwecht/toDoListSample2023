3afc0fa8854992cb86088fa2e2fab378567a9108
Matt Wecht <mattwecht@gmail.com>
2023-05-18 16:57:20 -0400

    Initial Commit

---
+++ toDoListSample2023/toDoListSample2023App.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListSample2023App.swift
+//  toDoListSample2023
+//
+//  Created by scholar on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListSample2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
